95b2c174b31d1db7d8105ffedcf2b4285b9cc491
Julia Farias <julia.farias2000@hotmail.com>
2021-10-27 16:14:01 -0300

    commit on develop

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-<<<<<<< HEAD
-# Create your views here.
-def homepage(request):
-    return render(request, 'pages/homepage.html' )
-=======
 from ads.models import Ad
 
 def homepage(request):
@@ -20,4 +15,3 @@ def homepage(request):
     }
 
     return render(request, 'pages/homepage.html', context)
->>>>>>> eac3cf274ba0aa8ca35eba82649b6d7e7c5f5460
